edb42443c3ae0740c006aa8e9deb0d08de858b75
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-09-12 16:43:29 +0200

    examples of conditional rendering

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,52 @@
-const Main = () => {
-    return <h1>This is the Main Section</h1>
-}
+import { useState } from "react";
+import movies from "../src/data/movies.json";
+import "./Main.css";
 
-export default Main
+const Main = () => {
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+  const deleteMovie = (id) => {
+    console.log("deleting th emovie", { id });
+
+    const newList = moviesToDisplay.filter((movie) => {
+      return movie.id !== id;
+    });
+
+    setMoviesToDisplay(newList);
+  };
+
+  return moviesToDisplay.map((movieObj) => {
+    const genreList = movieObj.genres.map((genre) => {
+      return <h4 key={genre}>{genre}</h4>;
+    });
+    return (
+      <div className="Main">
+        <h1>Number of Movies {moviesToDisplay.length}</h1>
+
+        <section key={movieObj.id} className="card">
+          <h2>Movie: {movieObj.title}</h2>
+
+        {movieObj.imgURL ? <img src={movieObj.imgURL} alt={movieObj.title} /> : <p>No picture available</p> }
+
+          
+
+          <h3>Rating: {movieObj.rating}</h3>
+          {movieObj.rating >8 && <p>RECOMMENDED</p>}
+          <h3>Year: {movieObj.year}</h3>
+          <h3>Genres:</h3>
+          {genreList}
+
+          <button
+            onClick={() => {
+              deleteMovie(movieObj.id);
+            }}
+          >
+            Delete
+          </button>
+        </section>
+      </div>
+    );
+  });
+};
+
+export default Main;
