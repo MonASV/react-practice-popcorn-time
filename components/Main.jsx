7e70459869fd489435dc2267d8f59b897befee17
Monica Sanchez <monicaa.sanchezv@gmail.com>
2023-09-13 11:40:34 +0200

    Lift up state to App component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,52 +1,28 @@
-import { useState } from "react";
-import movies from "../src/data/movies.json";
-import "./Main.css";
+import Movie from "./Movie";
 
-const Main = () => {
-  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+function Main(props){
 
-  const deleteMovie = (id) => {
-    console.log("deleting th emovie", { id });
+    // let message;
+    // if(moviesToDisplay.length > 0){
+    //     message = <h1>Number of movies: {moviesToDisplay.length}</h1>
+    // } else {
+    //     message = <h1>Sorry, no movies to display</h1>
+    // }
 
-    const newList = moviesToDisplay.filter((movie) => {
-      return movie.id !== id;
-    });
 
-    setMoviesToDisplay(newList);
-  };
-
-  return moviesToDisplay.map((movieObj) => {
-    const genreList = movieObj.genres.map((genre) => {
-      return <h4 key={genre}>{genre}</h4>;
-    });
     return (
-      <div className="Main">
-        <h1>Number of Movies {moviesToDisplay.length}</h1>
+        <div className="Main">
+            
+            {/* {message} */}
 
-        <section key={movieObj.id} className="card">
-          <h2>Movie: {movieObj.title}</h2>
-
-        {movieObj.imgURL ? <img src={movieObj.imgURL} alt={movieObj.title} /> : <p>No picture available</p> }
-
-          
-
-          <h3>Rating: {movieObj.rating}</h3>
-          {movieObj.rating >8 && <p>RECOMMENDED</p>}
-          <h3>Year: {movieObj.year}</h3>
-          <h3>Genres:</h3>
-          {genreList}
-
-          <button
-            onClick={() => {
-              deleteMovie(movieObj.id);
-            }}
-          >
-            Delete
-          </button>
-        </section>
-      </div>
+            {props.movies.map((movieObj) => {
+                return (
+                    <Movie key={movieObj.id} movieDetails={movieObj} callbackToDelete={props.callbackToDelete} />
+                )
+            })}
+            
+        </div>
     );
-  });
-};
+}
 
 export default Main;
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+import movies from "./data/movies.json";
+
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return ( <>
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
   
-  <Header />
-  <Main />
-  <Footer />
-  
-  </>  )
+  const deleteMovie = (movieId) => {
+    const newList = moviesToDisplay.filter( (element) => {
+        return element.id !== movieId;
+    });
+    setMoviesToDisplay(newList);
+  }
+
+
+  return (
+    <>
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <Main movies={moviesToDisplay} callbackToDelete={deleteMovie} />
+      <Footer />
+    </>
+  )
 }
 
 export default App
